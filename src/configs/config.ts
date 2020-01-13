04dac6fa9fcd268fea46f313c2796b48e65d4380
MikolaLvivUA <mikola.shidlovsky@gmail.com>
2020-01-13 15:03:20 +0200

    LMS-81 + add checkTestResult feature

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -8,7 +8,7 @@ export const config = {
     HOST: process.env.HOST || 'http://localhost',
 
     JWT_SECRET: process.env.JWT_SECRET || 'uf7e^Wai8efj32-&&620O10fm-32jfdj',
-    ACCESS_TOKEN_LIFETIME: process.env.ACCESS_TOKEN_LIFETIME || '999999m',
+    ACCESS_TOKEN_LIFETIME: process.env.ACCESS_TOKEN_LIFETIME || '10m',
 
     JWT_REFRESH_SECRET: process.env.PORT || '3f7e^fdf(*kjsd-&&620O10fm-333222fsd',
     REFRESH_TOKEN_LIFETIME: process.env.REFRESH_TOKEN_LIFETIME || '1h',
