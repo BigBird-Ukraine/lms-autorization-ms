d43ec21b3904a8f983fa251392b03e5492f2faf1
Viktor <viktor.kmin@eleks.com>
2020-01-15 14:14:59 +0200

    Remove string from services

--- src/services/oauth/oauth.service.ts
+++ src/services/oauth/oauth.service.ts
@@ -1,5 +1,6 @@
 import { model } from 'mongoose';
 
+import { DatabaseTablesEnum } from '../../constants';
 import { OauthToken, OauthTokenScheme, OauthTokenType } from '../../database';
 import { IOauthTokenModel, IUserFromTokenModel } from '../../interfaces';
 
@@ -11,25 +12,25 @@ class OAuthService {
     }
 
     deleteOauthTokenByAccessToken(access_token: string): Promise<void> {
-        const OauthTokenModel = model<OauthTokenType>('Oauth_token', OauthTokenScheme);
+        const OauthTokenModel = model<OauthTokenType>(DatabaseTablesEnum.OAUTH_TOKEN_COLLECTION_NAME, OauthTokenScheme);
 
         return OauthTokenModel.deleteOne({ access_token }) as any;
     }
 
     deleteOauthTokenByRefreshToken(refresh_token: string): Promise<void> {
-        const OauthTokenModel = model<OauthTokenType>('Oauth_token', OauthTokenScheme);
+        const OauthTokenModel = model<OauthTokenType>(DatabaseTablesEnum.OAUTH_TOKEN_COLLECTION_NAME, OauthTokenScheme);
 
         return OauthTokenModel.deleteOne({ refresh_token }) as any;
     }
 
     getUserFromAccessToken(access_token: string): Promise<IUserFromTokenModel> {
-        const OauthTokenModel = model<OauthTokenType>('Oauth_token', OauthTokenScheme);
+        const OauthTokenModel = model<OauthTokenType>(DatabaseTablesEnum.OAUTH_TOKEN_COLLECTION_NAME, OauthTokenScheme);
 
         return OauthTokenModel.findOne({ access_token }).populate('user_id').select({ user_id: 1, _id: 0 }) as any;
     }
 
     getUserFromRefreshToken(refresh_token: string): Promise<IUserFromTokenModel> {
-        const OauthTokenModel = model<OauthTokenType>('Oauth_token', OauthTokenScheme);
+        const OauthTokenModel = model<OauthTokenType>(DatabaseTablesEnum.OAUTH_TOKEN_COLLECTION_NAME, OauthTokenScheme);
 
         return OauthTokenModel.findOne({ refresh_token }).populate('user_id').select({ _id: 0 }) as any;
     }
--- src/services/question/question.service.ts
+++ src/services/question/question.service.ts
@@ -1,5 +1,6 @@
 import { model } from 'mongoose';
 
+import { DatabaseTablesEnum } from '../../constants';
 import { Question, QuestionSchema, QuestionType } from '../../database';
 import { IQuestion } from '../../interfaces';
 
@@ -11,7 +12,7 @@ class QuestionService {
   }
 
   getQuestions(limit: number, offset: number, sort: string, order?: string, filter?: any): Promise<IQuestion[]> {
-    const QuestionModel = model<QuestionType>('Question', QuestionSchema);
+    const QuestionModel = model<QuestionType>(DatabaseTablesEnum.QUESTION_COLLECTION_NAME, QuestionSchema);
     order = order === 'ASC' ? 'ASC' : 'DESC';
 
     return QuestionModel
@@ -25,7 +26,7 @@ class QuestionService {
   }
 
   async getMyQuestion(_id: string, limit: number, offset: number): Promise<IQuestion[]> {
-    const QuestionModel = model<QuestionType>('Question', QuestionSchema);
+    const QuestionModel = model<QuestionType>(DatabaseTablesEnum.QUESTION_COLLECTION_NAME, QuestionSchema);
 
     return QuestionModel
       .find({ user_id: `${_id}` }); // todo limit offset bug(dont queriing with this parameters)
@@ -33,20 +34,20 @@ class QuestionService {
   }
 
   async getQuestionById(questionId: string):  Promise<IQuestion> {
-    const QuestionModel = model<QuestionType>('Question', QuestionSchema);
+    const QuestionModel = model<QuestionType>(DatabaseTablesEnum.QUESTION_COLLECTION_NAME, QuestionSchema);
 
     return QuestionModel
       .findOne({ _id: `${questionId}` }) as any;
   }
 
   async getAnswersByQuestionId(questionId: string): Promise<IQuestion> {
-    const QuestionModel = model<QuestionType>('Question', QuestionSchema);
+    const QuestionModel = model<QuestionType>(DatabaseTablesEnum.QUESTION_COLLECTION_NAME, QuestionSchema);
 
     return QuestionModel.findById(questionId).select({answers: 1, _id: 0}) as any;
   }
 
   async deleteQuestionById(_id: string) {
-    const QuestionModel = model<QuestionType>('Question', QuestionSchema);
+    const QuestionModel = model<QuestionType>(DatabaseTablesEnum.QUESTION_COLLECTION_NAME, QuestionSchema);
 
     return QuestionModel.deleteOne({ _id });
   }
