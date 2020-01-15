717f41c1fc7e190458465dd3bd25adaf7ec1df5d
MikolaLvivUA <mikola.shidlovsky@gmail.com>
2020-01-15 14:57:44 +0200

    create getLEssonByID endpoint + fix comment

--- src/routes/lesson/lesson.router.ts
+++ src/routes/lesson/lesson.router.ts
@@ -16,13 +16,13 @@ router.use(checkAccessTokenMiddleware);
 router.get('/', lessonController.getLesson);
 router.get('/:lesson_id/test', lessonController.generateTestByLessonId);
 router.post('/:lesson_id/test', checkPassedTestData, userController.addTestResult);
+router.get('/:lesson_id', isLessonPresentMiddleware, lessonController.getLessonById);
 
 router.use(checkIsTeacher);
 router.post('/', lessonController.createLesson);
 router.get('/my', lessonController.getMyLesson);
 
 router.use('./:lesson_id', isLessonPresentMiddleware, isLessonOwnerMiddleware);
-router.get('/:lesson_id', lessonController.getLessonById);
 router.patch('/:lesson_id', lessonController.updateMyLesson);
 router.patch('/:lesson_id/question', checkQuestionsListLenght, isQuestionExistInLessonMiddleware, lessonController.addQuestionToLesson);
 router.delete('/:lesson_id', lessonController.deleteMyLesson);
