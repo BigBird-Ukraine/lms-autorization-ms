baf6fbb307daa6ba5e9a36220ceb0becd62847cc
Oleh Kosylo <kosylopoker@gmail.com>
2020-07-13 17:26:08 +0300

    LMS-168 Fix bug with pass data for test

--- src/middlewares/lesson/checkPassedTestData.middleware.ts
+++ src/middlewares/lesson/checkPassedTestData.middleware.ts
@@ -1,33 +1,32 @@
 import { NextFunction, Response } from 'express';
-import { IPassedTestData, IRequestExtended } from '../../interfaces';
+
+import { IRequestExtended } from '../../interfaces';
 import { questionService } from '../../services/question';
 
 export const checkPassedTestData = async (req: IRequestExtended, res: Response, next: NextFunction) => {
   try {
-    const { lesson_id } = req.params;
-    const { question_list } = req.body as IPassedTestData;
+    const {lesson_id} = req.params;
+    const question_list = req.body;
+
     const questions_id = [];
     let testResult = 0;
 
     for (const {question_id, chosen_answers} of question_list) {
+      const {answers} = await questionService.getAnswersByQuestionId(question_id);
 
-      const { answers } = await questionService.getAnswersByQuestionId(question_id);
       const correctAnswer = answers.filter(value => value.correct);
-
       let chosenCorrectQuestionCount = 0;
-
       questions_id.push(question_id);
 
-      chosen_answers.forEach(chosen_answer => {
+      for (const chosen_answer of chosen_answers) {
         answers.forEach(answer => {
           if (chosen_answer.toString() === answer._id.toString() && answer.correct) {
             chosenCorrectQuestionCount += 1;
           }
         });
-      });
+      }
 
       const percentageRatio = chosenCorrectQuestionCount / correctAnswer.length;
-
       testResult += +percentageRatio.toFixed(1) * 10;
     }
 
@@ -38,7 +37,6 @@ export const checkPassedTestData = async (req: IRequestExtended, res: Response, 
     };
 
     next();
-
   } catch (e) {
     next(e);
   }
